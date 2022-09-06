6e692b0c8617d934d11c24975d66ba0ab8c76d14
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-09-06 11:54:07 -0500

    embeding php in html

--- index.php
+++ index.php
@@ -1,7 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-$greeting = "Hello";
+<head>
+  <meta charset="UTC-8">
+  <title>Document</title>
+  <style>
+    header {
+      background: gray;
+      text-align: center;
+      padding: 2em;
+    }
+  </style>
+</head>
 
-$name = "Oscar Wankaew";
+<body>
+  <header>
+    <h1>
+      <!-- <?php echo "Hello, " . $_GET["name"]; ?> -->
+      <?= "Hello, " . htmlspecialchars($_GET["name"]); ?>
+    </h1>
+  </header>
+</body>
 
-echo "{$greeting}, {$name}.";
+</html>
